736a677a25b411439615601343b854916d1c495e
Stephen Doty <stephendoty826@yahoo.com>
2021-10-28 14:25:40 -0500

    committing lectures

--- 10-28_Sequelize/orm/app.js
+++ 10-28_Sequelize/orm/app.js
@@ -1,6 +1,7 @@
 
 const express = require("express")
 const app = express()
+const db = require("./models")
 const port = 3000
 
 app.set("view engine", "ejs")
@@ -9,6 +10,38 @@ app.use(express.static("public"))
 
 // routes
 app.use(require("./routes/index"))
+
+// db.user.create({firstName: "Vicky", lastName: "Webber", email: "vickey@dc.com"})
+// db.user.create({firstName: "Karen", lastName: "Johnson", email: "karen@dc.com"})
+// db.user.create({firstName: "Samantha", lastName: "Bough", email: "samantha@dc.com"})
+// db.user.create({firstName: "Sarah", lastName: "Samuelson", email: "sarah@dc.com"})
+
+const findAll = async() => {
+    // let records = await db.user.findAll() // array of objects
+
+    // records.forEach(user => {
+    //     console.log(`Name: ${user.firstName} ${user.lastName} - Email: ${user.email}`);
+    // })
+
+    // let result = await db.user.findByPk(5)
+    // console.log(`Name: ${result.firstName} ${result.lastName} - Email: ${result.email}`);
+
+    // let records = await db.user.findAll({where: {firstName: "Sarah"}})
+    // records.forEach(user => {
+    //     console.log(`Name: ${user.firstName}`);
+    // })
+
+    // let deletedID = await db.user.destroy({where: {id: 7}})// this returns either 0 or 1 (0 is false, 1 is true)
+
+    // console.log(deletedID);
+
+    let update = await db.user.update({lastName: "West"}, {where: {id: 8}}) // this returns either 0 or 1 (0 is false, 1 is true)
+
+    console.log(update);
+
+} 
+
+findAll()
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
